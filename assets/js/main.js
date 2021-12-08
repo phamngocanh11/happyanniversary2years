42aca09cb5a53a1cc09f3ad3eb61efd8819b2886
phamngocanh11 <95614789+phamngocanh11@users.noreply.github.com>
2021-12-08 13:18:03 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+new TypeIt(".content", {
+  strings: [
+    "Happy 2nd anniversary of our love <3 ",
+    "Since we fell in love, we've been together all the time, even though things weren't easy at times. ",
+    " but you and I have always believed in each other, I love you much.",
+    "I'm sorry because I'm so childish that I make you sad, sometimes I don't think about you. ",
+    "Thank you for always being with me and I want many more years like this.",
+    "I don't write in Vietnamese because it's a bit cheesy!!!!",
+    "NA love P.Tâm forever <333 ",
+  ],
+  speed: 100,
+  waitUntilVisible: true,
+}).go();
